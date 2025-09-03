3a30bcf119b3fdbc4714707846ffb53b2d5f4c85
kongexp <lcctju@163.com>
2025-09-03 13:39:33 +0800

    update workflow for multiple ligands, all with bias

--- unidock/unidock_engine/scripts/json2sdf.py
+++ unidock/unidock_engine/scripts/json2sdf.py
@@ -21,45 +21,49 @@ from rdkit import Chem
 from rdkit.Chem import rdMolAlign
 
 
-def tran_json_to_sdf(fp_res_json, fp_sdf, fp_res_sdf, noH=False, new_name=None):
+def tran_json_to_sdf(fp_res_json, fp_sdf, fp_res_sdf, noH=False, topN=None):
+    print("topN: ", topN)
     # fixme: The atom order in fp_res_json is different from the original fp_sdf
     """ Use RDKit to perform an align&rmsd process, without Hydrogen """
     with open(fp_res_json, "r") as f:
         json_res = json.load(f)
+    list_k = list(json_res.keys())
 
     # fp_sdf is the input ligand structure for UD2 engine. By now, contains no Hydrogen atoms
-    mol_ref = Chem.SDMolSupplier(str(fp_sdf), removeHs=noH)[0]
-    for key in mol_ref.GetPropNames():
-        mol_ref.ClearProp(key)
-
-    ligand_name = list(json_res.keys())[0]
-    assert len(ligand_name) > 0, "ligand_name not found!"
-    if new_name is None:
-        new_name = ligand_name
+    list_mol_ref = Chem.SDMolSupplier(str(fp_sdf), removeHs=noH)
 
     # create a writer
     writer = Chem.SDWriter(fp_res_sdf)
 
-    for idx, pose in enumerate(json_res[ligand_name]):
-        mol_res = Chem.Mol(mol_ref)  # new copy for each pose
-        mol_res.SetProp("_Name", f"{new_name}_pose_{idx}")
-        conf = mol_res.GetConformer()
+    for mol_ref, k in zip(list_mol_ref, list_k):
+        # clean the property of the reference molecule
+        for key in mol_ref.GetPropNames():
+            mol_ref.ClearProp(key)
 
-        coords_res = np.array(pose["coords"]).reshape(-1, 3)
-        for i in range(conf.GetNumAtoms()):
-            conf.SetAtomPosition(i, coords_res[i])
+        # write each pose in the json file to the sdf file
+        for idx, pose in enumerate(json_res[k]):
+            if topN is not None:
+                if idx >= topN:
+                    break
+            mol_res = Chem.Mol(mol_ref)  # new copy for each pose
+            mol_res.SetProp("_Name", f"{k}_pose_{idx}")
+            conf = mol_res.GetConformer()
 
-        # add energy property (assume energy value in pose dictionary)
-        if "energy" in pose:
-            # convert energy list to space-separated string
-            energy_str = " ".join([f"{e:.3f}" for e in pose["energy"]])
-            mol_res.SetProp("energy", energy_str)
+            coords_res = np.array(pose["coords"]).reshape(-1, 3)
+            for i in range(conf.GetNumAtoms()):
+                conf.SetAtomPosition(i, coords_res[i])
 
-        # output conf to sdf file
-        writer.write(mol_res)
+            # add energy property (assume energy value in pose dictionary)
+            if "energy" in pose:
+                # convert energy list to space-separated string
+                energy_str = " ".join([f"{e:.3f}" for e in pose["energy"]])
+                mol_res.SetProp("energy", energy_str)
 
-        # rmsd = rdMolAlign.CalcRMS(mol_ref, mol_res)
-        # list_rmsd.append(rmsd)
+            # output conf to sdf file
+            writer.write(mol_res)
+
+            # rmsd = rdMolAlign.CalcRMS(mol_ref, mol_res)
+            # list_rmsd.append(rmsd)
     writer.close()
     print(f"The sdf file {fp_res_sdf} has been generated.")
 
@@ -71,7 +75,7 @@ if __name__ == "__main__":
     parser.add_argument("reference_sdf", type=str, help="the reference sdf file")
     parser.add_argument("output_sdf", type=str, help="the output sdf file")
     parser.add_argument("--noH", action="store_true", help="whether to remove hydrogen atoms")
-    parser.add_argument("--new_name", type=str, help="the new name of the ligand")
+    parser.add_argument("--topN", type=int, help="the top N poses to write to the sdf file", default=None)
     args = parser.parse_args()
 
-    tran_json_to_sdf(args.json_file, args.reference_sdf, args.output_sdf, args.noH, args.new_name)
+    tran_json_to_sdf(args.json_file, args.reference_sdf, args.output_sdf, args.noH, args.topN)
